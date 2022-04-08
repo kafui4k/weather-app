abe57549bcc9bd0708e412e9be47166bff1fff29
Kafui Alordo <kalordo7@gmail.com>
2022-04-08 21:55:33 +0000

    Setup styling with webpack style, css-loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
+require('./styles.scss');
 console.log('weather app...');
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,25 @@ module.exports = {
                 test: /\.(js)$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
-            }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ]
     }
 };
